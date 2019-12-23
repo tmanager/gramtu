488104f06a5e5ec896929ead427e43e9b0088d74
zhangxiaodang <961590849@qq.com>
2019-12-23 18:43:56 +0800

    增加rmq配置

---
+++ gramtu-core/src/main/java/com/frank/gramtu/core/rmq/RmqConst.java
@@ -0,0 +1,7 @@
+package com.frank.gramtu.core.rmq;
+
+public class RmqConst {
+
+    // grammarly客户端通知队列名称
+    public static final String QUEUE_NAME_GRAMMARLY_CLIENT = "rpc.server.queue";
+}
--- gramtu-web/src/main/java/com/frank/gramtu/web/GramtuWebApplication.java
+++ gramtu-web/src/main/java/com/frank/gramtu/web/GramtuWebApplication.java
@@ -1,12 +1,17 @@
 package com.frank.gramtu.web;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.frank.gramtu.core.rmq.RmqService;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -23,4 +28,17 @@ public class GramtuWebApplication {
         SpringApplication.run(GramtuWebApplication.class, args);
     }
 
+    @Autowired
+    private RmqService rmqService;
+
+    @RequestMapping(value = "msg")
+    public void sendMsg() {
+
+        JSONObject json = new JSONObject();
+        json.put("ddbh", "zhangxd");
+        json.put("file_url","http://www.gramtu.com/group1/M00/00/00/rBBVI14AmPiARAF9AAAB9Puwo2k741.txt");
+
+        Object result = rmqService.rpcToGrammarly(json.toJSONString());
+        log.info("返回值为：{}", result);
+    }
 }
