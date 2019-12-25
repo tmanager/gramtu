0e080b8fa26d283eb64eedfb448a2ca0118de15f
zhangxiaodang <961590849@qq.com>
2019-12-25 11:12:52 +0800

    提交价格设置

---
+++ gramtu-web/src/main/java/com/frank/gramtu/web/gprice/GPriceRequest.java
@@ -0,0 +1,15 @@
+package com.frank.gramtu.web.gprice;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GPriceRequest {
+
+    private String price = "";
+
+    private String wordnum = "";
+
+    private String discount = "";
+}
