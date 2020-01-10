e43210fb05053c07a648b6720bcd9d4d0082f498
tmanager <961590849@qq.com>
2020-01-10 17:54:38 +0800

    增加重复率返回值

--- gramtu-core/src/main/java/com/frank/gramtu/core/bean/ResponseTurnitinBean.java
+++ gramtu-core/src/main/java/com/frank/gramtu/core/bean/ResponseTurnitinBean.java
@@ -8,54 +8,56 @@ import java.util.List;
 
 /**
  * 返回类.
- * 
+ *
  * @author 张孝党 2019/09/10.
- * 
  * @version V1.00.
- * 
- *          更新履历： V1.00 2019/09/10 张孝党 创建.
+ * <p>
+ * 更新履历： V1.00 2019/09/10 张孝党 创建.
  */
 @Getter
 @Setter
 public class ResponseTurnitinBean {
 
-	// 返回码
-	private String retcode = "0000";
+    // 返回码
+    private String retcode = "0000";
 
-	// 返回信息
-	private String retmsg = "正常完成";
+    // 返回信息
+    private String retmsg = "正常完成";
 
-	// 日志列表
-	private List<String> logList = new ArrayList<String>();
+    // 日志列表
+    private List<String> logList = new ArrayList<String>();
 
-	// 论文ID
-	private String thesisId = "";
+    // 论文ID
+    private String thesisId = "";
 
-	// 文档大小
-	private String fileSize = "";
+    // 文档大小
+    private String fileSize = "";
 
-	// 字数
-	private String wordCount = "";
+    // 字数
+    private String wordCount = "";
 
-	// 字符数
-	private String charCount = "";
+    // 字符数
+    private String charCount = "";
 
-	// 页数
-	private String pageCount = "";
+    // 页数
+    private String pageCount = "";
 
-	/**
-	 * 构造方法1.
-	 */
-	public ResponseTurnitinBean() {
+    // 重复率
+    private String rate = "";
 
-	}
+    /**
+     * 构造方法1.
+     */
+    public ResponseTurnitinBean() {
 
-	/**
-	 * 构造方法2.
-	 */
-	public ResponseTurnitinBean(String retCode, String retMsg) {
-		this.retcode = retCode;
-		this.retmsg = retMsg;
-	}
+    }
+
+    /**
+     * 构造方法2.
+     */
+    public ResponseTurnitinBean(String retCode, String retMsg) {
+        this.retcode = retCode;
+        this.retmsg = retMsg;
+    }
 
 }
